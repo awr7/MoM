2346daffd463546f57749cf9553f8f5edb1592d9
awr7 <147633136+awr7@users.noreply.github.com>
2024-05-12 16:30:19 -0400

    added claude and gemini

--- kingArchitecture.py
+++ kingArchitecture.py
@@ -1,3 +1,7 @@
+import anthropic
+import google.generativeai as genai
+
+genai.configure(api_key="")
 PINK = '\033[95m'
 CYAN = '\033[96m'
 YELLOW = '\033[93m'
@@ -7,3 +11,28 @@ RESET_COLOR = '\033[0m'
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
+    
+def claude():
+    client = anthropic.Anthropic(
+        # defaults to os.environ.get("ANTHROPIC_API_KEY")
+        api_key="",
+    )
+    message = client.messages.create(
+        model="claude-3-opus-20240229",
+        max_tokens=1000,
+        temperature=0.0,
+        system="Respond only in Yoda-speak.",
+        messages=[
+            {"role": "user", "content": "How are you today?"}
+        ]
+    )
+
+    print(message.content)
+
+def gemini():
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro")
+
+    convo = model.start_chat(history=[])
+
+    convo.send_message("Hello, how are you today?")
+    print(convo.last.text)
