e0bfc49cff0eb52be89989ff718981169359b1ee
scybersec <shimulcybersec@gmail.com>
2024-05-12 16:40:32 -0400

    replicate and openai

--- kingArchitecture.py
+++ kingArchitecture.py
@@ -1,5 +1,7 @@
 import anthropic
 import google.generativeai as genai
+import replicate
+import openai
 
 genai.configure(api_key="")
 PINK = '\033[95m'
@@ -36,3 +38,19 @@ def gemini():
 
     convo.send_message("Hello, how are you today?")
     print(convo.last.text)
+
+# The meta/meta-llama-3-70b-instruct model can stream output as it's running.
+for event in replicate.stream(
+    "meta/meta-llama-3-70b-instruct",
+    input={
+        "top_p": 0.9,
+        "prompt": "Work through this problem step by step:\n\nQ: Sarah has 7 llamas. Her friend gives her 3 more trucks of llamas. Each truck has 5 llamas. How many llamas does Sarah have in total?",
+        "max_tokens": 512,
+        "min_tokens": 0,
+        "temperature": 0.6,
+        "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+        "presence_penalty": 1.15,
+        "frequency_penalty": 0.2
+    },
+):
+    print(str(event), end="")
