8548c3206a446a4703687e227925712ade3e7eb3
awr7 <147633136+awr7@users.noreply.github.com>
2024-05-12 22:50:55 -0400

    cleaned code, added loading enviroment variables

--- kingArchitecture.py
+++ kingArchitecture.py
@@ -4,13 +4,16 @@ import replicate
 import os
 from dotenv import load_dotenv
 
+# Load the environment variables
 load_dotenv()
 
-
+# Set up the API keys
 replicate.api_token = os.getenv('REPLICATE_API_TOKEN')
+genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+anthropicKey = os.getenv('CLAUDE_API_KEY')
 
-genai.configure(api_key=os.getenv(''))
 
+# Terminal Colors
 PINK = '\033[95m'
 CYAN = '\033[96m'
 YELLOW = '\033[93m'
@@ -20,61 +23,61 @@ RESET_COLOR = '\033[0m'
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
-    
-# def claude():
-# client = anthropic.Anthropic(
-#     # defaults to os.environ.get("ANTHROPIC_API_KEY")
-#     api_key="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",
-# )
-# message = client.messages.create(
-#     model="claude-3-opus-20240229",
-#     max_tokens=1000,
-#     temperature=0.0,
-#     system="Respond only in Yoda-speak.",
-#     messages=[
-#         {"role": "user", "content": "How are you today?"}
-#     ]
-# )
 
-# print(message.content)
+# Run claude() to start a conversation with the Claude model
+def claude():
+    client = anthropic.Anthropic(api_key=anthropicKey)
+    message = client.messages.create(
+        model="claude-3-opus-20240229",
+        max_tokens=1000,
+        temperature=0.0,
+        system="Respond only in Yoda-speak.",
+        messages=[
+            {"role": "user", "content": "How are you today?"}
+        ]
+    )
 
-# def gemini():
-#     model = genai.GenerativeModel(model_name="gemini-1.0-pro")
+    print(message.content)
 
-#     convo = model.start_chat(history=[])
+# Run gemini() to start a conversation with the Gemini model
+def gemini():
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro")
 
-#     convo.send_message("Hello, how are you today?")
-#     print(convo.last.text)
+    convo = model.start_chat(history=[])
 
-# The meta/meta-llama-3-70b-instruct model can stream output as it's running.
+    convo.send_message("Hello, how are you today?")
+    print(PINK, convo.last.text, RESET_COLOR)
 
-
-# for event in replicate.stream(
-#     "meta/meta-llama-3-70b-instruct",
-#     input={
-#         "top_p": 0.9,
-#         "prompt": "Work through this problem step by step:\n\nQ: Sarah has 7 llamas. Her friend gives her 3 more trucks of llamas. Each truck has 5 llamas. How many llamas does Sarah have in total?",
-#         "max_tokens": 512,
-#         "min_tokens": 0,
-#         "temperature": 0.6,
-#         "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
-#         "presence_penalty": 1.15,
-#         "frequency_penalty": 0.2,
-#     },
-# ):
-#     print(str(event), end="")
-
-for event in replicate.stream(
-    "mistralai/mistral-7b-instruct-v0.2",
-    input={
-        "top_p": 0.9,
-        "prompt": "Give me 5 restaurants in Montclair area in New jersey for mothers day",
-        "max_tokens": 512,
-        "min_tokens": 0,
-        "temperature": 0.6,
-        "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
-        "presence_penalty": 1.15,
-        "frequency_penalty": 0.2,
-    },
-):
-    print(str(event), end="")
+# Run llama3() to start a conversation with the Llama model
+def llama3():
+    for event in replicate.stream(
+        "meta/meta-llama-3-70b-instruct",
+        input={
+            "top_p": 0.9,
+            "prompt": "Work through this problem step by step:\n\nQ: Sarah has 7 llamas. Her friend gives her 3 more trucks of llamas. Each truck has 5 llamas. How many llamas does Sarah have in total?",
+            "max_tokens": 512,
+            "min_tokens": 0,
+            "temperature": 0.6,
+            "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+            "presence_penalty": 1.15,
+            "frequency_penalty": 0.2,
+        },
+    ):
+        print(str(event), end="")
+        
+# Run mistralai() to start a conversation with the Mistral model       
+def mistralai():
+    for event in replicate.stream(
+        "mistralai/mistral-7b-instruct-v0.2",
+        input={
+            "top_p": 0.9,
+            "prompt": "Give me 5 restaurants in Montclair area in New jersey for mothers day",
+            "max_tokens": 512,
+            "min_tokens": 0,
+            "temperature": 0.6,
+            "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+            "presence_penalty": 1.15,
+            "frequency_penalty": 0.2,
+        },
+    ):
+        print(str(event), end="")
